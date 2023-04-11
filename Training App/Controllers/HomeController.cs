ca1d74a9bca11e36439cb3d21817174d41eb2092
AbdullahFarooq-ASE <129084463+AbdullahFarooq-ASE@users.noreply.github.com>
2023-04-11 07:27:55 -0700

    Initial commit

---
+++ Training App/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Training_App.Models;
+
+namespace Training_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Date> dates = new List<Date>();
+            for (int i = 0; i < 10; i++)
+            {
+                dates.Add(new Date() { date_time = i });
+            }
+            return View(dates);
+        }
+    }
+}
---
+++ Training App/Helpers/CustomHtmlHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Training_App.Helpers
+{
+    public static class CustomHtmlHelper
+    {
+        public static IHtmlString Img(this HtmlHelper htmlHelper, string src, string alt)
+        {
+            TagBuilder builder = new TagBuilder("img");
+            builder.Attributes.Add("src", src);
+            builder.Attributes.Add("alt", alt);
+            return new MvcHtmlString(builder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
